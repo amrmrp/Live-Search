6d5d30c501e2bd2181962159bd5de909f53794f9
amirm1379 <amirreza.moghaddampoor@gmail.com>
2021-08-28 22:04:57 +0430

    modified script.js getjson

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $.getJSON("data.json",
-    function (data, textStatus, jqXHR) {
-        console.log(data);
+    function (data) {
+       
+        var output='<ul class="searchresult">';
+        $.each(data, function (key, value) { 
+             output += '<li>';
+             output += '<h2>' + value.name +'</h2>';
+             output += '<img src="images/' +value.shortname +'_tn.jpg" alt="'+value.name +'"/>';
+             output +='<p>'+value.bio+'</p>';
+             output +='</li>';
+        });
+        output+='</ul>';
+       $('#update').html(output);
     }
 );
