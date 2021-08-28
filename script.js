003754c8ab51e77294cd9cb2bf5db1282daa5553
amirm1379 <amirreza.moghaddampoor@gmail.com>
2021-08-28 23:16:09 +0430

    modified script keyup and search

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-$.getJSON("data.json",
-    function (data) {
-       
-        var output='<ul class="searchresult">';
-        $.each(data, function (key, value) { 
-             output += '<li>';
-             output += '<h2>' + value.name +'</h2>';
-             output += '<img src="images/' +value.shortname +'_tn.jpg" alt="'+value.name +'"/>';
-             output +='<p>'+value.bio+'</p>';
-             output +='</li>';
-        });
-        output+='</ul>';
-       $('#update').html(output);
-    }
-);
+$(document).ready(function(){
+            $("#search").keyup(function(){
+            $("#search").css("background-color", "pink");
+            var searchfild=$("#search").val();
+            var myexp=new RegExp(searchfild,"i");
+        $.getJSON("data.json",
+            function (data) {
+                var output='<ul class="searchresult">';
+                $.each(data, function (key, value) { 
+                    if((value.name.search(myexp) !=-1)||
+                    (value.bio.search(myexp) !=-1)
+                    ){
+
+                   
+                    output += '<li>';
+                    output += '<h2>' + value.name +'</h2>';
+                    output += '<img src="images/' +value.shortname +'_tn.jpg" alt="'+value.name +'"/>';
+                    output +='<p>'+value.bio+'</p>';
+                    output +='</li>';
+
+
+                }
+                });
+                output+='</ul>';
+            $('#update').html(output);
+            });
+         });
+  });
